1fdd9d261adfd6b530fa481324f89283708c0d2e
Zamir Lalji <zamirlal15@gmail.com>
2023-05-17 17:38:19 -0400

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1 className='sm:text-4xl text-2xl font-bond my-6 text-gray-900'>About</h1>
+        <p className="mx-auto leading-relaxed text-base mb-4">Betty decided to write a short story and she was sure it was going to be 
+        amazing. She had already written it in her head and each time she thought about 
+        it she grinned from ear to ear knowing how wonderful it would be. She could 
+        imagine the accolades coming in and the praise she would receive for creating such 
+        a wonderful piece. She was therefore extremely frustrated when she actually sat 
+        down to write the short story and the story that was so beautiful inside her 
+        head refused to come out that way on paper.</p>
+        <p className="mx-auto leading-relaxed text-base mb-4">Betty decided to write a short story and she was sure it was going to be 
+        amazing. She had already written it in her head and each time she thought about 
+        it she grinned from ear to ear knowing how wonderful it would be. She could 
+        imagine the accolades coming in and the praise she would receive for creating such 
+        a wonderful piece. She was therefore extremely frustrated when she actually sat 
+        down to write the short story and the story that was so beautiful inside her 
+        head refused to come out that way on paper.</p>
+        <p className="mx-auto leading-relaxed text-base mb-4">There wasn't a whole lot more that could be done. It had become a wait-and
+        -see situation with the final results no longer in her control. That didn't 
+        stop her from trying to control the situation. She demanded that things be done 
+        as she desperately tried to control what couldn't be. "Are you getting my 
+        texts???" she texted to him. He glanced at it and chuckled under his breath. Of 
+        course he was getting them, but if he wasn't getting them, how would he ever be 
+        able to answer? He put the phone down and continued on his project. He was ignoring 
+        her texts and he planned to continue to do so.</p>
+    </div>
+  );
+};
+
+export default About;
